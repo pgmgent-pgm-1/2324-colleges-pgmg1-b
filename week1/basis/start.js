cd299bd2eb1a3cb4c832e9e3b5250447a986eb69
Michael Vanderpoorten <michael@yungpanda.com>
2023-09-18 15:57:48 +0200

    Update

--- week1/basis/start.js
+++ week1/basis/start.js
@@ -19,3 +19,11 @@ console.log(x + 2);
 const fullName = "Michael Vanderpoorten"; // string
 const birthYear = 1992; // number
 const isMarried = false; // boolean
+
+// string concatenation
+console.log("Mijn naam is " + fullName + " en ik ben " + age + " jaar oud.");
+console.log("Hierna komt een enter \n. Hallo van een andere lijn.");
+console.log(`Hierna komt een enter
+
+hallo van een andere lijn`);
+console.log(`Mijn naam is ${firstName} en ik ben ${age} jaar oud.`);
---
+++ week1/oefeningen/hello_pgm.js
@@ -0,0 +1,4 @@
+// voeg hieronder een lijn code toe
+const name = "Michael";
+// onderstaande lijn code pas je niet aan
+console.log("Hallo PGM, mijn naam is " + name);
