82d7f8c002312242dd4a269e3e3b22d7356be8bd
Michael Vanderpoorten <michael@yungpanda.com>
2023-09-19 16:06:59 +0200

    Les 2

---
+++ week1/basis/herhaling.js
@@ -0,0 +1,21 @@
+console.log("Hallo");
+
+const studentName = "Tyson"; // string
+const studentAge = 19; // number
+const isHappy = true; // boolean
+
+console.log(studentName + " is " + studentAge + " jaar oud");
+console.log(`${studentName} is ${studentAge} jaar oud`);
+
+// variables
+// camelCase
+const isStudentHappy = true;
+// snake case
+const is_student_happy = true;
+
+let text = "Dit is een tekst.";
+text = text + " Ander stukje tekst";
+// kortere versie
+text += " Ander stukje tekst";
+
+console.log(text);
---
+++ week1/basis/math.js
@@ -0,0 +1,8 @@
+let total = 1;
+total += 3;
+console.log(total);
+// total = total - 2;
+total -= 2;
+console.log(total);
+
+console.log(5 % 2);
