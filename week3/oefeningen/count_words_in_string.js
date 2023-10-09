f2fe4bcba826f691ad968ce0a5deca9d400174cc
Michael Vanderpoorten <michael@yungpanda.com>
2023-10-09 15:45:02 +0200

    Fix solution

--- week3/oefeningen/count_words_in_string.js
+++ week3/oefeningen/count_words_in_string.js
@@ -2,7 +2,7 @@ function calculateAmountOfWords(text) {
   text = text.replace(/[.,\/#!$%\^&\*;:{}=\-_~()]/g, "");
   const words = text.split(" ");
   return words.length;
-}of consa
+}
 
 const result = calculateAmountOfWords("Hallo! dit is een tekst in de les!");
 console.log(result);
